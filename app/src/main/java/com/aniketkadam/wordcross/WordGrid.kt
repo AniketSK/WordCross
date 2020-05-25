cdaeba1e5d49fc8d102430a8de2c8f17f75c171e
aniket <anikadamg@gmail.com>
2020-05-25 16:08:37 +0530

    Corrected dependencies, added the word grid fragment

---
+++ app/src/main/java/com/aniketkadam/wordcross/WordGrid.kt
@@ -0,0 +1,27 @@
+package com.aniketkadam.wordcross
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+
+
+class WordGrid : Fragment() {
+
+    private lateinit var viewModel: WordGridViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.word_grid_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(WordGridViewModel::class.java)
+    }
+
+}
---
+++ app/src/main/java/com/aniketkadam/wordcross/WordGridViewModel.kt
@@ -0,0 +1,7 @@
+package com.aniketkadam.wordcross
+
+import androidx.lifecycle.ViewModel
+
+class WordGridViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
