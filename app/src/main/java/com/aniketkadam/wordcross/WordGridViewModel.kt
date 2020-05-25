3b7f6d1caf6918f279cdf36a17b07b97675b6ee8
aniket <anikadamg@gmail.com>
2020-05-25 22:20:27 +0530

    Added a fragment to display the word grid

--- app/src/main/java/com/aniketkadam/wordcross/WordGridViewModel.kt
+++ app/src/main/java/com/aniketkadam/wordcross/WordGridViewModel.kt
@@ -3,5 +3,8 @@ package com.aniketkadam.wordcross
 import androidx.lifecycle.ViewModel
 
 class WordGridViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    override fun onCleared() {
+        super.onCleared()
+    }
 }
