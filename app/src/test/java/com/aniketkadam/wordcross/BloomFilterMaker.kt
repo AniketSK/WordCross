434c4ed757cb9a87187848d2ef11d16d6dc28a57
aniket <anikadamg@gmail.com>
2020-05-29 18:43:39 +0530

    Added tests and test files

---
+++ app/src/test/java/com/aniketkadam/wordcross/BloomFilterMaker.kt
@@ -0,0 +1,32 @@
+package com.aniketkadam.wordcross
+
+import com.google.common.hash.BloomFilter
+import com.google.common.hash.Funnels
+import org.junit.Ignore
+import org.junit.Test
+import java.io.File
+
+class BloomFilterMaker {
+
+    /**
+     * Un-ignore this test to generate a new set of bloom filter data to load at runtime
+     */
+    @Ignore
+    @Test
+    fun `make the bloom filter out of an existing text file`() {
+        val filter = BloomFilter.create(
+            Funnels.stringFunnel(Charsets.UTF_8),
+            200000, // Round up from the current list of 131k words
+            0.0001
+        )
+
+        ClassLoader.getSystemResourceAsStream("combined.txt")!!.bufferedReader().readLines()
+            .forEach {
+                filter.put(it)
+            }
+
+        val writeFile = File("src\\main\\res\\raw\\unicorn.txt").outputStream()
+        filter.writeTo(writeFile)
+        writeFile.close()
+    }
+}
---
+++ app/src/test/java/com/aniketkadam/wordcross/WordCheckerTest.kt
@@ -0,0 +1,25 @@
+package com.aniketkadam.wordcross
+
+import org.hamcrest.Matchers.equalTo
+import org.junit.Assert.*
+import org.junit.Test
+import java.io.InputStream
+
+class WordCheckerTest {
+
+    private val wc = WordChecker(object : WordDataLoader {
+        override fun loadData(): InputStream =
+            ClassLoader.getSystemResourceAsStream("unicorn.txt")!!
+    })
+
+    @Test
+    fun `common words are detected by the word checker`() {
+        assertThat(wc.isWord("DOG"), equalTo(true))
+        assertThat(wc.isWord("SLENDER"), equalTo(true))
+    }
+    
+    @Test
+    fun `strings that are not words are correctly detected as not present`() {
+        assertThat(wc.isWord("SLZK"), equalTo(false))
+    }
+}
