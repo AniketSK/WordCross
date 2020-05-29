d66fc96af7448e23cbb5c61e12385c9a66bc2fa1
aniket <anikadamg@gmail.com>
2020-05-29 21:18:41 +0530

    Keeping track of correct and incorrect words

--- app/src/main/java/com/aniketkadam/wordcross/WordGridViewModel.kt
+++ app/src/main/java/com/aniketkadam/wordcross/WordGridViewModel.kt
@@ -10,12 +10,35 @@ class WordGridViewModel(
 ) : ViewModel() {
 
     private val pWordGridDisplay = MutableLiveData<List<Char>?>()
+    private val pScore = MutableLiveData<Int>(0)
+    private val pCorrectWordsMade = MutableLiveData<List<String>>(emptyList())
+    private val pIncorrectWordsMade = MutableLiveData<List<String>>(emptyList())
+    private val pErrorEffect = SingleLiveEvent<String>()
 
     val wordGridDisplay: LiveData<List<Char>?>
         get() = pWordGridDisplay
 
+    val score: LiveData<Int>
+        get() = pScore
+
     fun startNewGame() {
         pWordGridDisplay.postValue(wordGrid.generateLetterGrid())
+    }
+
+    fun submitWord(word: String) {
+        if (wordChecker.isWord(word)) {
+            // Increment the score
+            pScore.value = pScore.value!! + 1
+            // Keep track of the words that were made
+            pCorrectWordsMade.value = pCorrectWordsMade.value?.plus(word)
+        } else {
+            reportError(word)
+        }
+    }
+
+    private fun reportError(word: String) {
+        pErrorEffect.postValue(word)
+        pIncorrectWordsMade.postValue(pIncorrectWordsMade.value?.plus(word))
     }
 
     override fun onCleared() {
