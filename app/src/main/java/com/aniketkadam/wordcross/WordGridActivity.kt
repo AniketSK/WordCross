2b55e08d946ef001ed8aa843a0279eafe88c2f6f
aniket <anikadamg@gmail.com>
2020-06-04 16:28:14 +0530

    Arranged a proto layout, Table isn't working

--- app/src/main/java/com/aniketkadam/wordcross/WordGridActivity.kt
+++ app/src/main/java/com/aniketkadam/wordcross/WordGridActivity.kt
@@ -4,30 +4,52 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
 import androidx.ui.core.setContent
+import androidx.ui.foundation.Border
 import androidx.ui.foundation.Text
+import androidx.ui.graphics.Color
+import androidx.ui.graphics.SolidColor
+import androidx.ui.layout.Column
+import androidx.ui.layout.Row
+import androidx.ui.material.Card
 import androidx.ui.tooling.preview.Preview
+import androidx.ui.unit.Dp
 import com.aniketkadam.wordcross.ui.AppTheme
 
 class WordGridActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AppTheme {
-                Greeting("Android")
+            WordGridScreen(listOf('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'))
+        }
+    }
+}
+
+@Composable
+fun WordGridScreen(letterList: List<Char>) {
+    AppTheme {
+        Column {
+            for (i in (0..8) step 3) {
+                Row {
+                    letterList.subList(i, i + 3).forEach {
+                        WordCard(name = it)
+                    }
+                }
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun WordCard(name: Char) {
+    Card(
+        border = Border(Dp.Hairline, SolidColor(Color.Black))
+    ) {
+        Text(name.toString())
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    AppTheme {
-        Greeting("Android")
-    }
+    WordGridScreen(listOf('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'))
 }
