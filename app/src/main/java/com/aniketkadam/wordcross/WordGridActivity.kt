aa7aa7811955421e8d0a8d3292e1dc07e9c9c1b4
aniket <anikadamg@gmail.com>
2020-06-04 17:17:48 +0530

    Done with compose

--- app/src/main/java/com/aniketkadam/wordcross/WordGridActivity.kt
+++ app/src/main/java/com/aniketkadam/wordcross/WordGridActivity.kt
@@ -3,17 +3,20 @@ package com.aniketkadam.wordcross
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
+import androidx.ui.core.Alignment
+import androidx.ui.core.Modifier
 import androidx.ui.core.setContent
+import androidx.ui.core.tag
 import androidx.ui.foundation.Border
 import androidx.ui.foundation.Text
 import androidx.ui.graphics.Color
 import androidx.ui.graphics.SolidColor
-import androidx.ui.layout.Column
-import androidx.ui.layout.Row
+import androidx.ui.layout.*
 import androidx.ui.material.Card
 import androidx.ui.tooling.preview.Preview
 import androidx.ui.unit.Dp
 import com.aniketkadam.wordcross.ui.AppTheme
+
 
 class WordGridActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,12 +30,24 @@ class WordGridActivity : AppCompatActivity() {
 @Composable
 fun WordGridScreen(letterList: List<Char>) {
     AppTheme {
-        Column {
-            for (i in (0..8) step 3) {
-                Row {
-                    letterList.subList(i, i + 3).forEach {
-                        WordCard(name = it)
-                    }
+        EntireWordGrid(letterList, "wordGrid")
+    }
+}
+
+@Composable
+fun EntireWordGrid(letterList: List<Char>, tag: String) {
+    Column(
+        modifier = Modifier.tag(tag) + Modifier.fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalGravity = Alignment.CenterHorizontally
+    ) {
+        for (i in (0..8) step 3) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalGravity = Alignment.CenterVertically
+            ) {
+                letterList.subList(i, i + 3).forEach {
+                    WordCard(name = it)
                 }
             }
         }
@@ -44,7 +59,7 @@ fun WordCard(name: Char) {
     Card(
         border = Border(Dp.Hairline, SolidColor(Color.Black))
     ) {
-        Text(name.toString())
+        Text(text = name.toString())
     }
 }
 
